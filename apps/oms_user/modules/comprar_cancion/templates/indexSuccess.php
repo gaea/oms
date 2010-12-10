2bf3f03205fde85fbe1ea9585a20514d23600c05
gustavo <gaea@gaea-desktop.(none)>
2010-12-10 18:06:15 -0500

    ignore

---
+++ apps/oms_user/modules/comprar_cancion/templates/indexSuccess.php
@@ -0,0 +1,4 @@
+<div id="div_form_comprar_cancion"></div>
+
+<?php echo javascript_include_tag('comprar_cancion/toolt_comprar_cancion.js') ?>
+<?php echo javascript_include_tag('comprar_cancion/form_comprar_cancion.js') ?>
