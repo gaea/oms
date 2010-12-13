9ce237641df1c1d86212859c58108c05cc1ee791
gustavo <gaea@gaea-desktop.(none)>
2010-12-13 03:16:28 -0500

    cambios interfaz publicar mensajes

--- web/js/publicar_mensaje/form_publicar_mensaje.js
+++ web/js/publicar_mensaje/form_publicar_mensaje.js
@@ -1,9 +1,9 @@
 
-	var publicar_mensaje_contenedor_panel = new Ext.FormPanel({
+	var publicar_mensaje_contenedor_panel = new Ext.Panel({
 		frame: true,
 		padding: '5px',
 		autoWidth: true,
-		height: 400,
+		height: 405,
 		border: true,
 		tabTip :'Aqui puedes publicar los mensajes a tus empleados',
 		monitorResize:true,
@@ -11,22 +11,31 @@
 		items: 
 		[
 			{
-				xtype: 'htmleditor',
-				anchor: '100%',
-				height: 340,
-				id: 'publicar_mensaje_mensaje_textfield',
-				name: 'publicar_mensaje_mensaje_textfield',
-				fieldLabel: 'Mensaje'
-			}
-		],
-		buttons:
-		[
-			{	
-				text: 'Publicar',
-				handler: fun_publicar_mensaje_publicar,
-				id: 'publicar_mensaje_publicar_boton',
-				iconCls: 'crear16',
-				tooltip: 'Pulse aqui para publicar el mensaje'
+				xtype: 'form',
+				title: 'Mensajes para los empleados',
+				frame: true,
+				padding: '5px',
+				items: 
+				[
+					{
+						xtype: 'htmleditor',
+						anchor: '100%',
+						height: 300,
+						id: 'publicar_mensaje_mensaje_textfield',
+						name: 'publicar_mensaje_mensaje_textfield',
+						fieldLabel: 'Mensaje'
+					}
+				],
+				buttons:
+				[
+					{	
+						text: 'Publicar',
+						handler: fun_publicar_mensaje_publicar,
+						id: 'publicar_mensaje_publicar_boton',
+						iconCls: 'crear16',
+						tooltip: 'Pulse aqui para publicar el mensaje'
+					}
+				]
 			}
 		],
 		renderTo:'div_form_publicar_mensaje'
